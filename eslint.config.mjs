5b81f802b960240d290a26520c6a10533cd9a6b3
Addy897 <145797684+Addy897@users.noreply.github.com>
2024-12-30 16:51:31 +0530

    student card and profile

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next',"next/core-web-vitals", "next/typescript"],
+    rules: {
+      // Disable `@typescript-eslint/no-explicit-any`
+      '@typescript-eslint/no-explicit-any': 'off',
+      
+      // Disable `react-hooks/exhaustive-deps` warning for missing dependencies in `useEffect`
+      'react-hooks/exhaustive-deps': 'off',
+  
+      // Disable `@next/next/no-img-element` warning for using <img> instead of <Image>
+      '@next/next/no-img-element': 'off',
+  
+      // Disable `no-var` rule to allow usage of `var`
+      'no-var': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
